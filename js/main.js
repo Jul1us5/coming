bf9315cb2b8c2484afeefa344901faf32aa92303
Ju1u5 <julius.vlasovas@gmail.com>
2021-04-26 20:45:40 +0300

    Burger

---
+++ js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Burger Meniu animation and VAR.
+//--------------------------------------------------
+let burger = document.querySelector('.burger');
+let navigation = document.querySelector('.nav');
+
+burger.addEventListener('click', () => {
+
+    if (navigation.className == 'nav on') {
+        navigation.classList.remove('on');
+    } else {
+        navigation.classList.add('on');
+    }
+
+});
+//--------------------------------------------------
